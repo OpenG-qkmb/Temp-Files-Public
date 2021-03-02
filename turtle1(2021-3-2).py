401f9e2b232120e839290a94d1556d07f14f3009
OpenG-qkmb <63486452+OpenG-qkmb@users.noreply.github.com>
2021-03-02 17:01:27 +0800

    添加2021年3月2日的turtle画图代码
    添加2021年3月2日的turtle画图代码

---
+++ turtle1(2021-3-2).py
@@ -0,0 +1,9 @@
+from turtle import *
+screensize(1024,512,"#CC0000")
+pencolor("#FFD700")
+pensize(4)
+for i in range(6):
+    forward(128)
+    right(60)
+#斯大林教导我们忠诚于人民，并鼓励我们去建立功勋。
+#                                                                                       ——《苏联颂（1944版）》
